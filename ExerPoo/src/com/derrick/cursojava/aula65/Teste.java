ffd8db8153da72e816fa978a2b14b3ca99ab9b25
DerrickCantrel <derrickcantrell16@gmail.com>
2020-04-06 14:20:42 -0300

    Aula 65

---
+++ ExerPoo/src/com/derrick/cursojava/aula65/Teste.java
@@ -0,0 +1,18 @@
+package com.derrick.cursojava.aula65;
+
+@InformacaoAula(
+		autor = "Derrick",
+		aulaNumero = 65
+		)
+public class Teste {
+
+	@InformacaoAula(
+			autor = "Derrick",
+			aulaNumero = 65
+			)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
