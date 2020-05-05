dc675c8349d2ace41e265f04ea6ea7f7d118c1ef
DerrickCantrel <derrickcantrell16@gmail.com>
2020-05-05 09:27:58 -0300

    Cars

---
+++ Carros/src/Principal.java
@@ -0,0 +1,26 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Carro c1 = new Carro();
+		c1.potencia = 10;
+		c1.nome = "Corolla";
+		c1.velocidade = 0;
+
+		c1.acelerar();
+		c1.acelerar();
+		c1.frear();
+		c1.imprimir();
+
+		Carro c2 = new Carro();
+		c2.potencia = 20;
+		c2.nome = "Civic";
+		c2.velocidade = 0;
+
+		c2.acelerar();
+		c2.acelerar();
+		c2.frear();
+		c2.imprimir();
+	}
+
+}
