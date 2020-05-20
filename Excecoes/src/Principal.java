3cb8a467f1ecb3da7128ece82e0b6ad13fe772fb
DerrickCantrel <derrickcantrell16@gmail.com>
2020-05-20 20:32:29 -0300

    exceptions

---
+++ Excecoes/src/Principal.java
@@ -0,0 +1,30 @@
+
+public class Principal {
+
+	public static void main(String[] args){
+		a(-100);
+
+		System.out.println("Terminando a execução de main");
+
+	}
+
+	public static void a(int i){
+		System.out.println("Executando a() com " +i);
+		try {
+			b(i);
+		} catch (Exception e) {
+			System.out.println("tratando a exceção: "+e.getMessage());
+		}
+		System.out.println("Terminando a execução de a");
+	}
+	
+	public static void b(int i) throws Exception {
+		System.out.println("Executando b() com " +i);
+		if (i > 0)
+			throw new Exception("mensagem"); 
+		//Só será executado se a exceção não for chamado.
+		System.out.println("Terminando a execução de b");
+
+	}
+	
+}
