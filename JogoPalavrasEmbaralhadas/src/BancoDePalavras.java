32f2abb03fbf179bbcd6a5955e5ae4346aea9059
DerrickCantrel <derrickcantrell16@gmail.com>
2020-05-21 20:15:59 -0300

    acess sucefull

--- JogoPalavrasEmbaralhadas/src/BancoDePalavras.java
+++ JogoPalavrasEmbaralhadas/src/BancoDePalavras.java
@@ -1,26 +1,36 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Random;
 
-public class BancoDePalavras {
-	static String palavraSort[] = new String[21];
+public class BancoDePalavras{
+	private Embaralhador sortido;
+	private String palavraSort[] = new String[21];
 	
-	public static void leitorPalavras(String caminho) throws IOException {
+	public BancoDePalavras() {}
+	
+	public void leitorPalavras(String caminho) throws IOException {
 		BufferedReader buffRead = new BufferedReader(new FileReader(caminho));
 		String linha;
 		int i = 0;
 		while (true) {
 			linha = buffRead.readLine();
 			palavraSort[i] = linha;
-			i++;
-			if (linha != null) {
-				System.out.println(linha);
-			} else {
+			if (linha == null) {
 				break;
 			}
+			i++;
 		}
 		buffRead.close();
 	}
 	
+	public String sortearPalavra() {
+		Random numSort = new Random();
+		int sort = numSort.nextInt(20) + 0;		
+		//System.out.println(palavraSort[sort]);
+		
+		//fb.palavraDividida(palavraSort[sort]); modo antingo
+		//this.sortido.sort1((palavraSort[sort]));
+		return palavraSort[sort];
+	}
 }
