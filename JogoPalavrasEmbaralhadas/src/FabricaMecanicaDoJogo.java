bd01d96f8d0373c01f16e025cc3645ade947abcb
DerrickCantrel <derrickcantrell16@gmail.com>
2020-05-22 22:23:34 -0300

    implments interface mecanicaDoJogo complete

--- JogoPalavrasEmbaralhadas/src/FabricaMecanicaDoJogo.java
+++ JogoPalavrasEmbaralhadas/src/FabricaMecanicaDoJogo.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 
-public class FabricaMecanicaDoJogo implements MecanincaDoJogo {
+public class FabricaMecanicaDoJogo implements MecanicaDoJogo {
 
 	private int chances;
 	private int pontos;
+	private int modo;
 	FabricaEmbaralhadores fb;
 	BancoDePalavras bp;
 	
@@ -29,7 +30,15 @@ public class FabricaMecanicaDoJogo implements MecanincaDoJogo {
 		this.chances = chances;
 	}
 
-	public void inicioJogo() {
+	public int getModo() {
+		return modo;
+	}
+
+	public void setModo(int modo) {
+		this.modo = modo;
+	}
+
+	public void iniciaJogo() {
 		//Lendo as palavras de um arquivo.
 		String caminho = "/Users/Derrick/eclipse-workspace/JogoPalavrasEmbaralhadas/palavra/palavras.txt";
 		try {
@@ -37,59 +46,55 @@ public class FabricaMecanicaDoJogo implements MecanincaDoJogo {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		bp.sortearPalavra();
+		//Palavra sorteada normal
+		String aux = bp.sortearPalavra();
+		//Método que irá dividir e chamar o embaralhador
+		fb.palavraDividida(aux, getModo());
+		System.out.println("A palavra embaralhada é: " + fb.getPalavraInvertida());
 	}
 	
-	public void acertou(String reposta) {
-		if (reposta.equalsIgnoreCase(fb.getPalavraInvertida())) {
-			setPontos(1);
+	public void acertou(String resposta) {
+		if (resposta.equalsIgnoreCase(bp.getPalavraAtual())) {
+			System.out.println("Parabéns você acertou!!! ");
+			System.out.println();
+			setPontos(getPontos() + 1);
+			System.out.println("Vamos para a próxima palavra");
+			novaPalavra();
 		} else {
 			setChances(getChances() - 1);
-			if (getChances() != 0) {
-				tenteNovamente(fb.getPalavraInvertida());
+			if (getChances() == 0) {
+				System.out.println("Fim de Jogo sua pontuação foi: " + getPontos());
+				System.out.println();
+				fimDeJogo();
 			} else {
-				//chama a qtd de acertos 
-				qtdAcertos();
+				System.out.println("Você ainda tem " + getChances() + " chances.");
 			}
 		}
 	}
 	
+	public void novaPalavra() {
+		//Palavra sorteada normal
+		String aux = bp.sortearPalavra();
+		//Método que irá dividir e chamar o embaralhador
+		fb.palavraDividida(aux, getModo());
+		System.out.println("A Nova palavra embaralhada é: " + fb.getPalavraInvertida());
+	}
+	
 	//Acertou quantas palavras
 	@Override
-	public int qtdAcertos() {
-		fb.getPalavraInvertida();
-		
-		
-		return 0;
-	}	
-	
-	@Override
-	public int pontuacaoFinal() {
-		
-		
-		return this.pontos;
+	public boolean fimDeJogo() {
+		return true;
 	}
 
 	@Override
 	public void modoDificil() {
-		bp.sortearPalavra(fb);;
-
+		iniciaJogo();
 	}
 
 	@Override
-	public void modoFacil() {
-		// TODO Auto-generated method stub
-
+	public void modoNormal() {
+		iniciaJogo();
 	}
 
-	@Override
-	public void tenteNovamente(String novamente) {
-		System.out.println("==========================");
-		System.out.println("Você pode tentar novamente");
-		System.out.println("==========================");
-		
-	}
-
-
+	
 }
