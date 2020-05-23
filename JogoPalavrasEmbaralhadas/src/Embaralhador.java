97fec77b1ef797ceec1704fb08868c2d0695e3ec
DerrickCantrel <derrickcantrell16@gmail.com>
2020-05-22 22:22:32 -0300

    interface embaralhador complete

--- JogoPalavrasEmbaralhadas/src/Embaralhador.java
+++ JogoPalavrasEmbaralhadas/src/Embaralhador.java
@@ -1,5 +1,5 @@
 public interface Embaralhador {
 	//Implementar dois tipos de embaralhadores
 	public abstract void sort1(String palavra);
-	public abstract String sort2(String palavra);
+	public abstract void sort2(String palavra);
 }
